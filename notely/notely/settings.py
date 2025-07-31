c4e09bac6f48d1b50c858838128d7274586e678e
Saw Yan Naing <sawyan820@gmail.com>
2025-07-31 20:22:29 +0630

    deployment setting

--- notely/notely/settings.py
+++ notely/notely/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -124,3 +124,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # for user model
 AUTH_USER_MODEL = "notes.User"
+
+ALLOWED_HOSTS = ['*']
+
+
